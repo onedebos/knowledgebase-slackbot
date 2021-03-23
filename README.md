b274c573c1fe11fb12e63aa8760506870e4e3e1c
Adebola Adeniran <adebola@Adebolas-MacBook-Pro.local>
2021-03-23 01:42:42 +0100

    feat: add /update command

---
+++ README.md
@@ -0,0 +1,15 @@
+## Knowledge base Slackbot example
+
+This is a demo Slack bot built using Slack's [Bolt APIs](https://slack.dev/bolt-js/tutorial/getting-started).
+
+## Getting started
+- Clone the repo to your machine.
+- Run `yarn install` to install all the libraries.
+- Create a new Slack application to get your `SLACK_SIGNING_SECRET` and `SLACK_BOT_TOKEN`.
+- Create a Slash command with the command `/knowledge`
+- Install the app to your workspace
+- Create a `.env` file and add the following.
+
+```
+SLACK_SIGNING_SECRET="YOUR SIGNING SECRET"
+SLACK_BOT_TOKEN="YOUR BOT TOKEN"
---
+++ app.js
@@ -0,0 +1,135 @@
+const { App } = require("@slack/bolt");
+require("dotenv").config();
+// Initializes your app with your bot token and signing secret
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+const fs = require("fs");
+let raw = fs.readFileSync("db.json");
+let faqs = JSON.parse(raw);
+
+app.command("/knowledge", async ({ command, ack, say }) => {
+  try {
+    await ack();
+    let message = { blocks: [] };
+    faqs.data.map((faq) => {
+      message.blocks.push(
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "*Question ❓*",
+          },
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: faq.question,
+          },
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "*Answer ✔️*",
+          },
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: faq.answer,
+          },
+        }
+      );
+    });
+    say(message);
+  } catch (error) {
+    console.log("err");
+    console.error(error);
+  }
+});
+
+app.message(/products/, async ({ command, say }) => {
+  try {
+    let message = { blocks: [] };
+    const productsFAQs = faqs.data.filter((faq) => faq.keyword === "products");
+
+    productsFAQs.map((faq) => {
+      message.blocks.push(
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "*Question ❓*",
+          },
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: faq.question,
+          },
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "*Answer ✔️*",
+          },
+        },
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: faq.answer,
+          },
+        }
+      );
+    });
+
+    say(message);
+  } catch (error) {
+    console.log("err");
+    console.error(error);
+  }
+});
+
+app.command("/update", async ({ command, ack, say }) => {
+  try {
+    await ack();
+    const data = command.text.split("|");
+
+    const newFAQ = {
+      keyword: data[0].trim(),
+      question: data[1].trim(),
+      answer: data[2].trim(),
+    };
+
+    // save data to db.json
+    fs.readFile("db.json", function (err, data) {
+      const json = JSON.parse(data);
+      json.data.push(newFAQ);
+      fs.writeFile("db.json", JSON.stringify(json), function (err) {
+        if (err) throw err;
+        console.log("Successfully saved to db.json!");
+      });
+    });
+
+    say(`You've added a new FAQ with the keyword *${newFAQ.keyword}.*`);
+  } catch (error) {
+    console.log("err");
+    console.error(error);
+  }
+});
+
+(async () => {
+  const port = 3000;
+
+  // Start your app
+  await app.start(process.env.PORT || port);
+  console.log(`⚡️ Slack Bolt app is running on port ${port}!`);
+})();
