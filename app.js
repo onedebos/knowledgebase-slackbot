52e2b34a5e66f629290d45f4317b9c9e70d0d0ae
Adebola Adeniran <adebola@Adebolas-MacBook-Pro.local>
2021-03-31 10:55:05 +0100

    chore: update app.js to use socketMode

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ require("dotenv").config();
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
+  // socketMode:true, enable to use socket mode
+  // appToken: process.env.APP_TOKEN
 });
 
 const fs = require("fs");
